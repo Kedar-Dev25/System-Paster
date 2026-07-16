9490f11df6e52e9167d60adceabdca63136a38cd
Kedar-Dev25 <kedar.code7@gmail.com>
2026-07-16 21:41:58 +0530

    Finalized the UI

--- frontend-/src/component/Connect.jsx
+++ frontend-/src/component/Connect.jsx
@@ -1,14 +1,10 @@
 import { useParams } from "react-router-dom";
 
 function Connect() {
-
     const { sid } = useParams();
-
     console.log(sid);
-
     return (
         <h1>{sid}</h1>
     );
 }
-
 export default Connect;
